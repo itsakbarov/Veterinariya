cd5b7d7aa565d3e104dd77ec7af52d0ee834a02b
Sardor <asrorjon5538@gmail.com>
2020-12-07 02:08:57 +0500

    finally)))

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,15 +1,18 @@
-$(document).ready(function () {
-    $('.pet_logo').click(function (event) {
+$(document).ready(function() {
+    $('.pet_logo').click(function(event) {
         $('.overlay').toggleClass('opened');
         $('.modal').toggle(300);
         event.preventDefault();
+        $('body').css('overflow', 'hidden')
     });
-    $('.modal-x').click(function (event) {
+    $('.modal-x').click(function(event) {
         $('.modal').toggle(300);
         $('.overlay').toggleClass('opened');
+        $('body').css('overflow', 'unset')
         event.preventDefault();
     });
-    $('.overlay').click(function () {
+    $('.overlay').click(function() {
+        $('body').css('overflow', 'unset')
         $('.modal').toggle(300);
         $('.overlay').toggleClass('opened');
 
