861f2be71b1759d8f7cb067bc742ec0c7226063a
Sardor <asrorjon5538@gmail.com>
2020-12-07 02:57:56 +0500

    pop up added)

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,20 +1,27 @@
-$(document).ready(function() {
-    $('.pet_logo').click(function(event) {
+$(document).ready(function () {
+    $('.pet_logo').click(function (event) {
         event.preventDefault();
         $('.overlay').toggleClass('opened');
         $('.modal').toggle(300);
         $('body').css('overflow', 'hidden')
     });
-    $('.modal-x').click(function(event) {
+    $('.modal-x').click(function (event) {
         event.preventDefault();
         $('.modal').toggle(300);
         $('body').css('overflow', 'unset')
         $('.overlay').toggleClass('opened');
     });
-    $('.overlay').click(function() {
+    $('.overlay').click(function () {
         $('.modal').toggle(300);
         $('body').css('overflow', 'unset')
         $('.overlay').toggleClass('opened');
 
+    });
+    $('.pop-close').click(function () {
+        $('#popup').fadeOut(350)
     })
+    setTimeout(function () {
+        $('#popup').toggleClass('active')
+    }, 2000);
+    setTimeout()
 });
